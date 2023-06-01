d8f8123dc43a866b7d8b09923f2a9df4a2e8b715
rurigrass <ruripg@gmail.com>
2023-06-01 22:50:30 +0200

    added client side session provider

---
+++ app/context/AuthContext.tsx
@@ -0,0 +1,13 @@
+"use state";
+
+import { SessionProvider } from "next-auth/react";
+
+type Props = {
+  children?: React.ReactNode;
+};
+
+const Provider = ({ children }: Props) => {
+  return <SessionProvider>{children}</SessionProvider>;
+};
+
+export default Provider;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
+import { getServerSession } from 'next-auth'
 import Image from 'next/image'
+import { authOptions } from './api/auth/[...nextauth]/route';
 
-export default function Home() {
+
+export default async function Home() {
+  const session = await getServerSession(authOptions)
+  console.log(session);
+  
   return (
     <section>
       <h1>Home</h1>
+      <h1>Server Side Rendered</h1>
+      <pre>{JSON.stringify(session)}</pre>
     </section>
   )
 }
