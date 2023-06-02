1fa7045b750ff0b375e4149ddf154d90a75c98b5
rurigrass <ruripg@gmail.com>
2023-06-02 16:32:53 +0200

    only uses userName / removed name

--- app/(site)/register/page.tsx
+++ app/(site)/register/page.tsx
@@ -1,15 +1,66 @@
+"use client";
+import { useState } from "react";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
+type dataProps = {
+  userName: string;
+  email: string;
+  password: string;
+};
+
 const Register = () => {
+  const router = useRouter();
+  const [data, setData] = useState<dataProps>({
+    userName: "",
+    email: "",
+    password: "",
+  });
+
+  const registerUser = async (e: any) => {
+    e.preventDefault();
+    axios
+      .post("/api/register", data)
+      .then(() => alert("user has been registered"))
+      .catch(() => alert("An error has occured"));
+
+    console.log(e);
+  };
+
   return (
     <>
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <h2 className=" text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Sign in to your account
+            Register a new account
           </h2>
         </div>
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form className="space-y-6" action="#" method="POST">
+          <form className="space-y-6" onSubmit={registerUser}>
+  
+            <div>
+              <label
+                htmlFor="userName"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Username
+              </label>
+              <div className="mt-2">
+                <input
+                  id="userName"
+                  name="userName"
+                  type="text"
+                  required
+                  value={data.name}
+                  onChange={(e) =>
+                    setData({ ...data, userName: e.target.value })
+                  }
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
             <div>
               <label
                 htmlFor="email"
@@ -24,6 +75,8 @@ const Register = () => {
                   type="email"
                   autoComplete="email"
                   required
+                  value={data.email}
+                  onChange={(e) => setData({ ...data, email: e.target.value })}
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
@@ -53,6 +106,10 @@ const Register = () => {
                   type="password"
                   autoComplete="current-password"
                   required
+                  value={data.password}
+                  onChange={(e) =>
+                    setData({ ...data, password: e.target.value })
+                  }
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
@@ -63,7 +120,7 @@ const Register = () => {
                 type="submit"
                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                Sign in
+                Register
               </button>
             </div>
           </form>
